c8fce8e27ac717bb4823b51bcc1c8c37916f5023
SanjayMarathi <sanjaymarathiiii@email.com>
2026-05-27 12:44:43 +0530

    update for deployment

--- database.py
+++ database.py
@@ -1,13 +1,20 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 import os
+import json
 
-cred_path = "firebase-key.json"
 if not firebase_admin._apps:
-    if os.path.exists(cred_path):
-        cred = credentials.Certificate(cred_path)
+    env_cred = os.getenv("FIREBASE_KEY_JSON")
+    if env_cred:
+        cred_dict = json.loads(env_cred)
+        cred = credentials.Certificate(cred_dict)
         firebase_admin.initialize_app(cred)
     else:
-        raise Exception(f"Firebase credentials not found at {cred_path}")
+        cred_path = "firebase-key.json"
+        if os.path.exists(cred_path):
+            cred = credentials.Certificate(cred_path)
+            firebase_admin.initialize_app(cred)
+        else:
+            raise Exception(f"Firebase credentials not found in env FIREBASE_KEY_JSON or file {cred_path}")
 
 db = firestore.client()
